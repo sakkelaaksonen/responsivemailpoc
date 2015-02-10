c18b9c109677f3bc2c4576a5a72a9e10c53b1dfd
Sakke Laaksonen <sakke.laaksonen@gmail.com>
2015-02-10 10:41:08 +0200

    premailer helper app with sinatra

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@
 source 'https://rubygems.org'
 gem 'sinatra'
 gem  'shotgun'
+gem 'premailer'
+gem 'hpricot'
+gem 'nokogiri'
--- sinatra_helper_app/app.rb
+++ sinatra_helper_app/app.rb
@@ -1,21 +1,47 @@
 
 require 'sinatra/base'
+require 'premailer'
 
 class MailerApp < Sinatra::Application
 
 
-configure do
-  # setting one option
-  set :root , File.dirname( __FILE__)
-
-end
-
-
-  get '/' do
-  
-  erb :styleguide_layout
+  configure do
+    # setting one option
+    set :root , File.dirname( __FILE__)
 
   end
 
+
+  helpers do
+    #Pun intended
+    def premaul(filename)
+    Premailer.new(
+      File.open(filename),
+      # erb( :styleguide_layout),
+      {with_html_string: true}
+      ).to_inline_css
+    end
+
+  end
+  before '/' do
+    #Set template instance vars.
+    #True by default. Add ! to revert
+    @edge = (params[:edge].nil?)
+    @use_device_width = (params[:use_device_width].nil?)
+    @media_queries = (params[:media_queries].nil?)
+    @svg = (params[:svg].nil?)
+
+  end
+
+  get '/' do
+    erb( 
+      premaul("#{settings.views}/styleguide_layout.erb")
+      )
+  end
+
+  # after '/' do
+  #   premailer = Premailer.new(response.body,with_html_string: true)
+  #   response.body = premailer.to_inline_css
+  # end
 
 end
